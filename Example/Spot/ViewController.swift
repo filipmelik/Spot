5e5f220383e0807dd74f31ef3af528f862d8d377
Filip Melik <filip.melik@heureka.cz>
2017-04-24 07:59:05 +0200

    update example - add Spot invocation button

--- Example/Spot/ViewController.swift
+++ Example/Spot/ViewController.swift
@@ -20,6 +20,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func launchSpotWithButtonPress(_ sender: Any) {
+        
+        if let button = sender as? UIButton {
+            button.isHidden = true
+            print("Button was set to hidden, so it does not appear in the screenshot.")
+        }
+        
+        Spot.sharedInstance.launch()
+    }
 
 }
 
